f4c269bce84147a6c23202115dd2aa6adcc411cf
Kaimund <kai@kaimund.com>
2023-09-02 15:07:44 +0100

    Fixed unhandled promise rejection when offline

--- src/main/discord/discord.ts
+++ src/main/discord/discord.ts
@@ -4,8 +4,11 @@
     Copyright (C) 2023 Kaimund
 */
 
+import { AppLog } from '../helpers/AppLog';
 import { ShardingManager } from 'discord.js';
 
 const manager = new ShardingManager('./dist/main/discord/shard.js', { token: process.env.BOT_TOKEN });
 
-manager.spawn();
+manager.spawn().catch(error => {
+    AppLog.error(error, 'Spawning a new shard');
+});
